93d0155b5ef9b03b544618af1f52c03f622d5f2f
Daniyar Shaymardanov <105869759+Chainichek@users.noreply.github.com>
2023-05-28 22:46:01 +0400

    Набросок формы настроек

---
+++ Settings.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace PianinoGame
+{
+    public partial class Settings : Form
+    {
+        public Settings()
+        {
+            InitializeComponent();
+        }
+    }
+}
